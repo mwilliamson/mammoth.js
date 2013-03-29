53e7181e009efcdcad182d485972bfbfda982fc0
Michael Williamson <mike@zwobble.org>
2013-03-29 21:36:57 +0000

    Allow HTML elements to be reused in HTML generation

--- tests/html-generator.tests.js
+++ tests/html-generator.tests.js
@@ -14,13 +14,26 @@ describe('HtmlGenerator', function() {
     test('HTML-escapes text', function() {
         var generator = new HtmlGenerator();
         generator.text("<");
-        return assert.equal("&lt;", generator.asString());
+        return assert.equal(generator.asString(), "&lt;");
     });
     
     test('asString closes all elements', function() {
         var generator = new HtmlGenerator();
         generator.style(styles.elements(["p", "span"]));
         generator.text("Hello!");
-        return assert.equal("<p><span>Hello!</span></p>", generator.asString());
+        return assert.equal(generator.asString(), "<p><span>Hello!</span></p>");
+    });
+    
+    test('can leave some HTML elements for next style', function() {
+        var generator = new HtmlGenerator();
+        var listStyle = styles.elements([
+            styles.element("ul", {fresh: false}),
+            styles.element("li", {fresh: true})
+        ])
+        generator.style(listStyle);
+        generator.text("Apple");
+        generator.style(listStyle);
+        generator.text("Banana");
+        return assert.equal(generator.asString(), "<ul><li>Apple</li><li>Banana</li></ul>");
     });
 })
