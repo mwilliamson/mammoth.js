d3a8fc6cf15038553f0d67e79c03afb64e826644
Michael Williamson <mike@zwobble.org>
2013-03-29 21:11:44 +0000

    Test that asString closes all elements

--- tests/html-generator.tests.js
+++ tests/html-generator.tests.js
@@ -2,6 +2,7 @@ var assert = require("assert");
 var test = require("./testing").test;
 
 var HtmlGenerator = require("../lib/html-generator").HtmlGenerator;
+var styles = require("../lib/styles");
 
 
 describe('HtmlGenerator', function() {
@@ -14,5 +15,12 @@ describe('HtmlGenerator', function() {
         var generator = new HtmlGenerator();
         generator.text("<");
         return assert.equal("&lt;", generator.asString());
-    })
+    });
+    
+    test('asString closes all elements', function() {
+        var generator = new HtmlGenerator();
+        generator.style(styles.elements(["p", "span"]));
+        generator.text("Hello!");
+        return assert.equal("<p><span>Hello!</span></p>", generator.asString());
+    });
 })
