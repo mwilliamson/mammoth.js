15aaddae7a7e2dd8d369b514218778931a1543e0
Michael Williamson <mike@zwobble.org>
2013-03-29 20:31:19 +0000

    Convert XML into simple document elements

---
+++ tests/documents.tests.js
@@ -0,0 +1,28 @@
+var assert = require("assert");
+
+var documents = require("../lib/documents");
+var DocumentConverter = documents.DocumentConverter;
+var test = require("./testing").test;
+
+
+describe('DocumentConverter', function() {
+    test('should convert document containing one paragraph to single p element', function() {
+        var document = new documents.Document([
+            paragraphOfText("Hello.")
+        ]);
+        var converter = new DocumentConverter();
+        return converter.convertToHtml(document).then(function(result) {
+            assert.equal("<p>Hello.</p>", result.html);
+        });
+    })
+});
+
+function paragraphOfText(text) {
+    var run = runOfText(text);
+    return new documents.Paragraph([run]);
+}
+
+function runOfText(text) {
+    var textElement = new documents.Text(text);
+    return new documents.Run([textElement]);
+}
