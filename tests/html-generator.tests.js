257aee36fef663731ad29924b296ee518bf6e68f
Michael Williamson <mike@zwobble.org>
2013-03-29 21:09:48 +0000

    Add test for HtmlGenerator.text and HTML-escaping

--- tests/html-generator.tests.js
+++ tests/html-generator.tests.js
@@ -8,5 +8,11 @@ describe('HtmlGenerator', function() {
     test('generates empty string when newly created', function() {
         var generator = new HtmlGenerator();
         return assert.equal("", generator.asString());
+    });
+    
+    test('HTML-escapes text', function() {
+        var generator = new HtmlGenerator();
+        generator.text("<");
+        return assert.equal("&lt;", generator.asString());
     })
 })
