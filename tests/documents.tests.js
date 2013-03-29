0f90a6de310fd86472c29e945f06fe94c666fa2c
Michael Williamson <mike@zwobble.org>
2013-03-29 21:04:09 +0000

    Add styles with nested elements

--- tests/documents.tests.js
+++ tests/documents.tests.js
@@ -51,6 +51,20 @@ describe('DocumentConverter', function() {
             assert.equal("<h1>Hello.</h1>", result.html);
         });
     });
+    
+    test('can use stacked styles to generate nested HTML elements', function() {
+        var document = new documents.Document([
+            paragraphOfText("Hello.", "Heading1")
+        ]);
+        var converter = new DocumentConverter({
+            paragraphStyleMap: {
+                "Heading1": styles.elements(["h1", "span"])
+            }
+        });
+        return converter.convertToHtml(document).then(function(result) {
+            assert.equal("<h1><span>Hello.</span></h1>", result.html);
+        });
+    });
 });
 
 function paragraphOfText(text, styleName) {
