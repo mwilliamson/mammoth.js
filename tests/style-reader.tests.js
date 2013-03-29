354a6ba8f34108236879c8af4280abea74409be5
Michael Williamson <mike@zwobble.org>
2013-03-29 23:04:44 +0000

    Allow reading of multiple classes on style

--- tests/style-reader.tests.js
+++ tests/style-reader.tests.js
@@ -12,4 +12,18 @@ describe('styleReader.read', function() {
     test('reads nested elements', function() {
         assert.deepEqual(styleReader.read("ul li"), styles.elements(["ul", "li"]));
     });
+    
+    test('reads class on element', function() {
+        var expected = styles.elements([
+            styles.element("p", {"class": "tip"})
+        ])
+        assert.deepEqual(styleReader.read("p.tip"), expected);
+    });
+    
+    test('reads multiple classes on element', function() {
+        var expected = styles.elements([
+            styles.element("p", {"class": "tip help"})
+        ])
+        assert.deepEqual(styleReader.read("p.tip.help"), expected);
+    });
 })
