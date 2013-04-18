58d57f995fcf2077c3c4147dfc637cb1759de98b
Michael Williamson <mike@zwobble.org>
2013-04-18 19:58:19 +0100

    Display messages on demo

--- browser-demo/demo.js
+++ browser-demo/demo.js
@@ -3,6 +3,20 @@
         document.getElementById("document"),
         function(result) {
             document.getElementById("output").innerHTML = result.html;
+            
+            var messageHtml = result.messages.map(function(message) {
+                return '<li class="' + message.type + '">' + escapeHtml(message.message) + "</li>";
+            }).join("");
+            
+            document.getElementById("messages").innerHTML = "<ul>" + messageHtml + "</ul>";
         }
     );
+
+    function escapeHtml(value) {
+        return value
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+    }
 })();
