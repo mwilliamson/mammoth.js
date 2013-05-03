dc4025cae3964d0300b421b1b7ff486d9f4c5faf
Michael Williamson <mike@zwobble.org>
2013-05-03 21:32:30 +0100

    Rename style-reader.read to readHtmlPath

--- tests/style-reader.tests.js
+++ tests/style-reader.tests.js
@@ -4,33 +4,36 @@ var styleReader = require("../lib/style-reader");
 var test = require("./testing").test;
 
 
-describe('styleReader.read', function() {
+var readHtmlPath = styleReader.readHtmlPath;
+
+
+describe('styleReader.readHtmlPath', function() {
     test('reads single element', function() {
-        assert.deepEqual(styleReader.read("p"), htmlPaths.elements(["p"]));
+        assert.deepEqual(readHtmlPath("p"), htmlPaths.elements(["p"]));
     });
     
     test('reads nested elements', function() {
-        assert.deepEqual(styleReader.read("ul > li"), htmlPaths.elements(["ul", "li"]));
+        assert.deepEqual(readHtmlPath("ul > li"), htmlPaths.elements(["ul", "li"]));
     });
     
     test('reads class on element', function() {
         var expected = htmlPaths.elements([
             htmlPaths.element("p", {"class": "tip"})
         ])
-        assert.deepEqual(styleReader.read("p.tip"), expected);
+        assert.deepEqual(readHtmlPath("p.tip"), expected);
     });
     
     test('reads multiple classes on element', function() {
         var expected = htmlPaths.elements([
             htmlPaths.element("p", {"class": "tip help"})
         ])
-        assert.deepEqual(styleReader.read("p.tip.help"), expected);
+        assert.deepEqual(readHtmlPath("p.tip.help"), expected);
     });
     
     test('reads when element must be fresh', function() {
         var expected = htmlPaths.elements([
             htmlPaths.element("p", {}, {"fresh": true})
         ])
-        assert.deepEqual(styleReader.read("p:fresh"), expected);
+        assert.deepEqual(readHtmlPath("p:fresh"), expected);
     });
 })
