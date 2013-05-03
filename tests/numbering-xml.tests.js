49f72cd7bf6034f646a0f0e51ffbd204d86bea54
Michael Williamson <mike@zwobble.org>
2013-05-03 17:47:50 +0100

    Read numFmt from numbering.xml

--- tests/numbering-xml.tests.js
+++ tests/numbering-xml.tests.js
@@ -1,15 +1,34 @@
 var assert = require("assert");
 
+var duck = require("duck");
+
 var readNumberingXml = require("../lib/numbering-xml").readNumberingXml;
 var XmlElement = require("../lib/xmlreader").Element;
 var test = require("./testing").test;
 
 
 describe('readNumberingXml', function() {
-    test('reads no numbering elements from empty XML', function() {
+    test('w:num element inherits levels from w:abstractNum', function() {
         var numbering = readNumberingXml({
-            root: new XmlElement("w:numbering")
+            root: new XmlElement("w:numbering", {}, [
+                new XmlElement("w:abstractNum", {"w:abstractNumId": "42"}, [
+                    new XmlElement("w:lvl", {"w:ilvl": "0"}, [
+                        new XmlElement("w:numFmt", {"w:val": "bullet"})
+                    ]),
+                    new XmlElement("w:lvl", {"w:ilvl": "1"}, [
+                        new XmlElement("w:numFmt", {"w:val": "decimal"})
+                    ])
+                ]),
+                new XmlElement("w:num", {"w:numId": "47"}, [
+                    new XmlElement("w:abstractNumId", {"w:val": "42"})
+                ])
+            ])
         });
-        assert.deepEqual(numbering, []);
+        duck.assertThat(numbering.findLevel("47", "0"), duck.hasProperties({
+            isOrdered: false
+        }));
+        duck.assertThat(numbering.findLevel("47", "1"), duck.hasProperties({
+            isOrdered: true
+        }));
     });
 });
