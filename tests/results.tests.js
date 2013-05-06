e1d1373cda7fcf66aff7a89883c10ed9a8f7ffc7
Michael Williamson <mike@zwobble.org>
2013-05-06 19:43:44 +0100

    Remove duplicate messages when combining results

---
+++ tests/results.tests.js
@@ -0,0 +1,17 @@
+var assert = require("assert");
+
+var results = require("../lib/results");
+var Result = results.Result;
+var test = require("./testing").test;
+
+
+describe("Result.combine", function() {
+    test("removes any duplicate messages", function() {
+        var first = new Result(null, [results.warning("Warning...")]);
+        var second = new Result(null, [results.warning("Warning...")]);
+        
+        var combined = Result.combine([first, second]);
+        
+        assert.deepEqual(combined.messages, [results.warning("Warning...")]);
+    });
+});
