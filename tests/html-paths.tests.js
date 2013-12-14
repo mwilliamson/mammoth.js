cd79d48d9799b715188a63461f2023eb4187ad39
Michael Williamson <mike@zwobble.org>
2013-12-14 15:43:30 +0000

    Consider attributes when matching elements

--- tests/html-paths.tests.js
+++ tests/html-paths.tests.js
@@ -11,5 +11,12 @@ describe("html-paths", function() {
         assert.ok(pathPart.matchesElement({tagName: "ol"}));
         assert.ok(!pathPart.matchesElement({tagName: "p"}));
     });
+    
+    test("element matches if attributes are the same", function() {
+        var pathPart = htmlPaths.element(["p"], {"class": "tip"});
+        assert.ok(!pathPart.matchesElement({tagName: "p"}));
+        assert.ok(!pathPart.matchesElement({tagName: "p", attributes: {"class": "tip help"}}));
+        assert.ok(pathPart.matchesElement({tagName: "p", attributes: {"class": "tip"}}));
+    });
 });
 
