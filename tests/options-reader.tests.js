bfc33eec2b0a22e4bb0ee2412ec22df636134330
Michael Williamson <mike@zwobble.org>
2014-01-07 22:29:04 +0000

    Append standard style map to custom style map

--- tests/options-reader.tests.js
+++ tests/options-reader.tests.js
@@ -1,5 +1,6 @@
 var assert = require("assert");
 
+var style = require("../").style;
 var optionsReader = require("../lib/options-reader");
 var standardOptions = optionsReader._standardOptions;
 var readOptions = optionsReader.readOptions;
@@ -13,4 +14,16 @@ describe('readOptions', function() {
     it('standard options are used if options is empty', function() {
         assert.deepEqual(standardOptions, readOptions({}));
     });
+    
+    it('custom style mappings are prepended to standard style mappings', function() {
+        var options = readOptions({
+            styleMap: "p.SectionTitle => h2"
+        });
+        assert.deepEqual(style("p.SectionTitle => h2"), options.styleMap[0]);
+        assert.deepEqual(standardOptions.styleMap, options.styleMap.slice(1));
+    });
 });
+
+function last(array) {
+    return array[array.length - 1];
+}
