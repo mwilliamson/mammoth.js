80508a1a9828b5051e5514200d299c08698430b5
Michael Williamson <mike@zwobble.org>
2014-05-04 18:52:14 +0100

    Add test for unzipping a simple zip file

--- tests/unzip.tests.js
+++ tests/unzip.tests.js
@@ -1,4 +1,5 @@
 var assert = require("assert");
+var path = require("path");
 
 var test = require("./testing").test;
 var unzip = require("../lib/unzip");
@@ -9,6 +10,15 @@ describe("unzip", function() {
             assert.ok(false, "Expected failure");
         }, function(error) {
             assert.equal("Could not find file in options", error.message);
+        });
+    });
+    
+    test("unzip can open local zip file", function() {
+        var zipPath = path.join(__dirname, "test-data/hello.zip");
+        return unzip.openZip({path: zipPath}).then(function(zipFile) {
+            return zipFile.read("hello", "utf8");
+        }).then(function(contents) {
+            assert.equal(contents, "Hello world\n");
         });
     });
 });
