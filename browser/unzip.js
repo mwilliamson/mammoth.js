0c11e5675f9a0d4e263afed413aa656c20734f77
Michael Williamson <mike@zwobble.org>
2014-05-07 10:41:44 +0100

    Use jszip on server-side as well as browser-side

--- browser/unzip.js
+++ browser/unzip.js
@@ -1,34 +1,12 @@
-var JSZip = require("jszip");
-
 var promises = require("../lib/promises");
+var zipfile = require("../lib/zipfile");
 
 exports.openZip = openZip;
 
 function openZip(options) {
     if (options.arrayBuffer) {
-        return openArrayBuffer(options.arrayBuffer);
+        return zipfile.openArrayBuffer(options.arrayBuffer);
     } else {
         return promises.reject(new Error("Could not find file in options"));
     }
-}
-
-function openArrayBuffer(arrayBuffer) {
-    var zipFile = new JSZip(arrayBuffer);
-    function exists(name) {
-        return zipFile.file(name) !== null;
-    }
-    
-    function read(name, encoding) {
-        var array = zipFile.file(name).asUint8Array();
-        var buffer = new Buffer(array);
-        if (encoding) {
-            return promises.when(buffer.toString(encoding));
-        } else {
-            return promises.when(buffer);
-        }
-    }
-    return {
-        exists: exists,
-        read: read
-    };
 }
