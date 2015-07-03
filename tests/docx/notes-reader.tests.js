27f3d32cabc5fae5b163ddf5a5449d93ebd959d5
Michael Williamson <mike@zwobble.org>
2015-07-03 22:31:29 +0100

    Read note bodies immediately using relationships

--- tests/docx/notes-reader.tests.js
+++ tests/docx/notes-reader.tests.js
@@ -1,21 +1,24 @@
 var assert = require("assert");
 
-var readFootnotesXml = require("../../lib/docx/notes-reader").readFootnotesXml;
+var createFootnotesReader = require("../../lib/docx/notes-reader").createFootnotesReader;
+var BodyReader = require("../../lib/docx/body-reader").BodyReader;
+var documents = require("../../lib/documents");
 var XmlElement = require("../../lib/xmlreader").Element;
 var test = require("../testing").test;
 
 
 describe('readFootnotesXml', function() {
     test('ID and body of footnote are read', function() {
+        var bodyReader = new BodyReader({});
         var footnoteBody = [new XmlElement("w:p", {}, [])];
-        var footnotes = readFootnotesXml({
+        var footnotes = createFootnotesReader(bodyReader)({
             root: new XmlElement("w:footnotes", {}, [
                 new XmlElement("w:footnote", {"w:id": "1"}, footnoteBody)
             ])
         });
-        assert.equal(footnotes.length, 1);
-        assert.strictEqual(footnotes[0].body, footnoteBody);
-        assert.deepEqual(footnotes[0].id, "1");
+        assert.equal(footnotes.value.length, 1);
+        assert.deepEqual(footnotes.value[0].body, [new documents.Paragraph([])]);
+        assert.deepEqual(footnotes.value[0].noteId, "1");
     });
 });
 
@@ -25,12 +28,12 @@ footnoteTypeIsIgnored('separator');
 function footnoteTypeIsIgnored(type) {
     describe('footnotes of type ' + type + ' are ignored', function() {
         test('ID and body of footnote are read', function() {
-            var footnotes = readFootnotesXml({
+            var footnotes = createFootnotesReader()({
                 root: new XmlElement("w:footnotes", {}, [
                     new XmlElement("w:footnote", {"w:id": "1", "w:type": type}, [])
                 ])
             });
-            assert.equal(footnotes.length, 0);
+            assert.equal(footnotes.value.length, 0);
         });
     });
 }
