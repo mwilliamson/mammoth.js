42ea88fd99174ec0b86a71d5cd18244ea22dcadd
Michael Williamson <mike@zwobble.org>
2015-08-16 16:38:33 +0100

    Fix: browser version of openZip does not always return a promise

--- browser/unzip.js
+++ browser/unzip.js
@@ -5,7 +5,7 @@ exports.openZip = openZip;
 
 function openZip(options) {
     if (options.arrayBuffer) {
-        return zipfile.openArrayBuffer(options.arrayBuffer);
+        return promises.resolve(zipfile.openArrayBuffer(options.arrayBuffer));
     } else {
         return promises.reject(new Error("Could not find file in options"));
     }
