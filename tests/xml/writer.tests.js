578ff57e4b86c4b31268e8b22a401c7bfaa9fe1a
Michael Williamson <mike@zwobble.org>
2015-08-16 14:51:06 +0100

    Write text nodes

--- tests/xml/writer.tests.js
+++ tests/xml/writer.tests.js
@@ -1,29 +1,39 @@
 var assert = require("assert");
 
-var Element = require("../../lib/xml").Element;
+var xml = require("../../lib/xml");
 var writer = require("../../lib/xml/writer");
 var test = require("../testing").test;
 
 
 describe('xml.writer', function() {
     test('writing empty root element writes out xml declaration and empty root element', function() {
-        assertXmlString(new Element("root"), '<root/>');
+        assertXmlString(xml.element("root"), '<root/>');
     });
     
     test('can write empty child elements', function() {
-        assertXmlString(new Element("root", {}, [new Element("album"), new Element("single")]),
+        assertXmlString(xml.element("root", {}, [xml.element("album"), xml.element("single")]),
             '<root><album/><single/></root>');
     });
     
     test('can write empty descendant elements', function() {
-        var element = new Element("root", {}, [
-            new Element("album", {}, [
-                new Element("year"),
-                new Element("song")
+        var element = xml.element("root", {}, [
+            xml.element("album", {}, [
+                xml.element("year"),
+                xml.element("song")
             ])
         ]);
         assertXmlString(element,
             '<root><album><year/><song/></album></root>');
+    });
+    
+    test('can write text nodes', function() {
+        var element = xml.element("root", {}, [
+            xml.element("album", {}, [
+                xml.text("Everything in Transit")
+            ])
+        ]);
+        assertXmlString(element,
+            '<root><album>Everything in Transit</album></root>');
     });
 });
 
