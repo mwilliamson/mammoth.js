024d8735f21ae0d0e0e82d15b79db3ceac027d16
Michael Williamson <mike@zwobble.org>
2015-08-16 14:02:03 +0100

    Add test for writing to zip files

---
+++ tests/zipfile.tests.js
@@ -0,0 +1,26 @@
+var assert = require("assert");
+
+var JSZip = require("jszip");
+
+var test = require("./testing").test;
+var zipfile = require("../lib/zipfile");
+
+describe("zipfile", function() {
+    test('file in zip can be read after being written', function() {
+        var zip = emptyZipFile();
+        assert(!zip.exists("song/title"));
+        
+        zip.write("song/title", "Dark Blue");
+        
+        assert(zip.exists("song/title"));
+        return zip.read("song/title", "utf8").then(function(contents) {
+            assert.equal(contents, "Dark Blue");
+        });
+    });
+});
+
+function emptyZipFile() {
+    var zip = new JSZip();
+    var buffer = zip.generate({type: "arraybuffer"});
+    return zipfile.openArrayBuffer(buffer);
+}
