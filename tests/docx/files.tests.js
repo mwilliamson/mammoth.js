ced42637087b386df99cfd017262afad03b809ce
Michael Williamson <mike@zwobble.org>
2015-09-06 17:48:55 +0100

    Parse file URIs on Windows

--- tests/docx/files.tests.js
+++ tests/docx/files.tests.js
@@ -27,7 +27,8 @@ describe("Files", function() {
 
 describe("uriToPath", function() {
     test("leading slash is retained on non-Windows file URIs", function() {
-        assert.equal(uriToPath("file:///a/b/c"), "/a/b/c");
+        assert.equal(uriToPath("file:///a/b/c", "linux"), "/a/b/c");
+        assert.equal(uriToPath("file:///a/b/c", "win32"), "/a/b/c");
     });
     
     test("URI is unquoted", function() {
@@ -40,5 +41,14 @@ describe("uriToPath", function() {
     
     test("when host is set but not localhost then path cannot be found", function() {
         assert.throws(function() { uriToPath("file://example/a/b/c"); }, /Could not convert URI to path: file:\/\/example\/a\/b\/c/);
+    });
+    
+    test("leading slash is not dropped on Windows file URIs when platform is not Windows", function() {
+        assert.equal(uriToPath("file:///c:/a", "linux"), "/c:/a");
+    });
+    
+    test("leading slash is dropped on Windows file URIs when platform is Windows", function() {
+        assert.equal(uriToPath("file:///c:/a", "win32"), "c:/a");
+        assert.equal(uriToPath("file:///C:/a", "win32"), "C:/a");
     });
 });
