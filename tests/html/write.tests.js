9de6bed1cd4f24f591c4aa2a3012501bd792c157
Michael Williamson <mike@zwobble.org>
2015-10-14 19:26:10 +0100

    Don't escape double quotes outside of attributes

--- tests/html/write.tests.js
+++ tests/html/write.tests.js
@@ -12,10 +12,22 @@ describe("HTML write", function() {
             "&lt;&gt;&amp;");
     });
     
+    test("double quotes outside of attributes are not escaped", function() {
+        assert.equal(
+            generateString(html.text('"')),
+            '"');
+    });
+    
     test("element attributes are HTML escaped", function() {
         assert.equal(
             generateString(html.freshElement("p", {"x": "<"})),
             '<p x="&lt;"></p>');
+    });
+    
+    test("double quotes inside attributes are escaped", function() {
+        assert.equal(
+            generateString(html.freshElement("p", {"x": '"'})),
+            '<p x="&quot;"></p>');
     });
     
     test("element children are written", function() {
