e81f83906d26ace7c38b0b7b24e0d5f98da35172
Michael Williamson <mike@zwobble.org>
2017-01-23 20:23:03 +0000

    Handle base-relative URIs entirely in entry name generation

--- test/zipfile.tests.js
+++ test/zipfile.tests.js
@@ -17,48 +17,24 @@ test('file in zip can be read after being written', function() {
     });
 });
 
-test('leading slash in path is stripped if set when reading', function() {
-    var zip = emptyZipFile();
-    assert(!zip.exists("/song/title"));
-    
-    zip.write("song/title", "Dark Blue");
-    
-    assert(zip.exists("/song/title"));
-    return zip.read("/song/title", "utf8").then(function(contents) {
-        assert.equal(contents, "Dark Blue");
-    });
-});
-
-test('leading slash in path is stripped if set when writing', function() {
-    var zip = emptyZipFile();
-    assert(!zip.exists("song/title"));
-    
-    zip.write("/song/title", "Dark Blue");
-    
-    assert(zip.exists("song/title"));
-    return zip.read("song/title", "utf8").then(function(contents) {
-        assert.equal(contents, "Dark Blue");
-    });
-});
-
 function emptyZipFile() {
     var zip = new JSZip();
     var buffer = zip.generate({type: "arraybuffer"});
     return zipfile.openArrayBuffer(buffer);
 }
 
-test("joinZipPath", {
-    "joins relative path to base using forward slash": function() {
+test("uriToZipEntryName", {
+    "when path does not have leading slash then path is resolved relative to base": function() {
         assert.equal(
-            zipfile.joinZipPath("one/two", "three/four"),
+            zipfile.uriToZipEntryName("one/two", "three/four"),
             "one/two/three/four"
         )
     },
 
-    "joining base to absolute path ignores base": function() {
+    "when path has leading slash then base is ignored": function() {
         assert.equal(
-            zipfile.joinZipPath("one/two", "/three/four"),
-            "/three/four"
+            zipfile.uriToZipEntryName("one/two", "/three/four"),
+            "three/four"
         )
     }
 });
