b6ab371b1f004a627f7390c442b6406d1f2be499
Michael Williamson <mike@zwobble.org>
2021-08-27 19:42:39 +0100

    Update JSZip to 3.2.0

--- test/zipfile.tests.js
+++ test/zipfile.tests.js
@@ -6,21 +6,23 @@ var zipfile = require("../lib/zipfile");
 var test = require("./test")(module);
 
 test('file in zip can be read after being written', function() {
-    var zip = emptyZipFile();
-    assert(!zip.exists("song/title"));
-    
-    zip.write("song/title", "Dark Blue");
-    
-    assert(zip.exists("song/title"));
-    return zip.read("song/title", "utf8").then(function(contents) {
-        assert.equal(contents, "Dark Blue");
+    return emptyZipFile().then(function(zip) {
+        assert(!zip.exists("song/title"));
+
+        zip.write("song/title", "Dark Blue");
+
+        assert(zip.exists("song/title"));
+        return zip.read("song/title", "utf8").then(function(contents) {
+            assert.equal(contents, "Dark Blue");
+        });
     });
 });
 
 function emptyZipFile() {
     var zip = new JSZip();
-    var buffer = zip.generate({type: "arraybuffer"});
-    return zipfile.openArrayBuffer(buffer);
+    return zip.generateAsync({type: "arraybuffer"}).then(function(arrayBuffer) {
+        return zipfile.openArrayBuffer(arrayBuffer);
+    });
 }
 
 
