bffdcda7eb215a636cfa9a17cc1af57620bd6822
Michael Williamson <mike@zwobble.org>
2023-01-29 11:26:02 +0000

    Remove use of .done() in demo

--- browser-demo/demo.js
+++ browser-demo/demo.js
@@ -1,35 +1,36 @@
 (function() {
     document.getElementById("document")
         .addEventListener("change", handleFileSelect, false);
-        
+
     function handleFileSelect(event) {
         readFileInputEventAsArrayBuffer(event, function(arrayBuffer) {
             mammoth.convertToHtml({arrayBuffer: arrayBuffer})
-                .then(displayResult)
-                .done();
+                .then(displayResult, function(error) {
+                    console.error(error);
+                });
         });
     }
-    
+
     function displayResult(result) {
         document.getElementById("output").innerHTML = result.value;
-        
+
         var messageHtml = result.messages.map(function(message) {
             return '<li class="' + message.type + '">' + escapeHtml(message.message) + "</li>";
         }).join("");
-        
+
         document.getElementById("messages").innerHTML = "<ul>" + messageHtml + "</ul>";
     }
-    
+
     function readFileInputEventAsArrayBuffer(event, callback) {
         var file = event.target.files[0];
 
         var reader = new FileReader();
-        
+
         reader.onload = function(loadEvent) {
             var arrayBuffer = loadEvent.target.result;
             callback(arrayBuffer);
         };
-        
+
         reader.readAsArrayBuffer(file);
     }
 
