2e234c7d6ef381423b4f7d0ea5dac92dfce58a6b
Michael Williamson <mike@zwobble.org>
2023-05-21 11:50:07 +0100

    Don't read paragraph properties as document element

--- test/docx/comments-reader.tests.js
+++ test/docx/comments-reader.tests.js
@@ -2,13 +2,14 @@ var assert = require("assert");
 
 var createCommentsReader = require("../../lib/docx/comments-reader").createCommentsReader;
 var createBodyReader = require("../../lib/docx/body-reader").createBodyReader;
+var stylesReader = require("../../lib/docx/styles-reader");
 var documents = require("../../lib/documents");
 var xml = require("../../lib/xml");
 var test = require("../test")(module);
 
 
 function readComment(element) {
-    var bodyReader = createBodyReader({});
+    var bodyReader = createBodyReader({styles: stylesReader.defaultStyles});
     var commentsReader = createCommentsReader(bodyReader);
     var comments = commentsReader(element);
     assert.equal(comments.value.length, 1);
