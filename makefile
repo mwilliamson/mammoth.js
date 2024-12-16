d47357cf74cff30f85d298e237fa07bd687502fb
Jasper De Moor <jasperdemoor@gmail.com>
2024-12-16 16:49:47 +0100

    simplify

--- makefile
+++ makefile
@@ -7,9 +7,4 @@ setup: npm-install mammoth.browser.min.js
 
 npm-install:
 	npm install
-
-mammoth.browser.js:
-	node_modules/.bin/browserify lib/index.js --standalone mammoth -p browserify-prepend-licenses > $@
-
-mammoth.browser.min.js: mammoth.browser.js
-	node_modules/.bin/uglifyjs mammoth.browser.js -c > $@
+	
---
+++ test/extract-ast.tests.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const {extractNodeList} = require('../lib/index');
+
+const fixtures = path.join(__dirname, "fixtures");
+
+async function extractAst(input) {
+  const extracted = await extractNodeList({
+    path: input,
+  });
+  console.log(extracted);
+}
+
+const items = fs.readdirSync(fixtures);
+
+describe("extractAst", () => {
+  for (const item of items) {
+    if (item.endsWith(".docx")) {
+      it(`extractAst: ${item}`, async () => {
+        await extractAst(path.join(fixtures, item));
+      });
+    }
+  }
+})
