e72e7e3eb27a43bb1e91939aacda51b5730c34f7
Michael Williamson <mike@zwobble.org>
2025-05-31 09:05:05 +0100

    Remove promises.resolve()

--- browser/unzip.js
+++ browser/unzip.js
@@ -1,11 +1,10 @@
-var promises = require("../lib/promises");
 var zipfile = require("../lib/zipfile");
 
 exports.openZip = openZip;
 
 function openZip(options) {
     if (options.arrayBuffer) {
-        return promises.resolve(zipfile.openArrayBuffer(options.arrayBuffer));
+        return Promise.resolve(zipfile.openArrayBuffer(options.arrayBuffer));
     } else {
         return Promise.reject(new Error("Could not find file in options"));
     }
