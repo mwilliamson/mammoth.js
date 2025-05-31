64d0ed6e293f2a3a7ed2c3be97a805f02724693d
Michael Williamson <mike@zwobble.org>
2025-05-31 09:46:11 +0100

    Test rejected value in props()

--- test/promises.tests.js
+++ test/promises.tests.js
@@ -82,6 +82,21 @@ test("props", {
         });
     },
 
+    "when value is rejected promise then props is rejected promise": function() {
+        return promises.props({
+            a: promises.resolve(1),
+            b: promises.reject(new Error("failure")),
+            c: promises.resolve(3)
+        }).then(
+            function(result) {
+                assert.fail("Expected rejection");
+            },
+            function(error) {
+                assert.strictEqual(error.message, "failure");
+            }
+        );
+    },
+
     "props(...).also(...) can be used to add props": function() {
         return promises.props({
             a: promises.resolve(1)
